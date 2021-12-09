e1c1c212a8a56846dae25498b4b103da73e12360
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2021-12-09 21:11:04 +0200

    Use Routing + Add about and footer components

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean;
   subscription: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private route: Router) {
     this.subscription = this.uiService.onToggle().subscribe((value) => {
       this.showAddTask = value;
     });
@@ -24,5 +25,9 @@ export class HeaderComponent implements OnInit {
   // Add method to toggle add task
   toggleAddTask() {
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.route.url === route;
   }
 }
